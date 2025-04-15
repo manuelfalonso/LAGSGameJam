a9ab07d8e7b02320b0f8cfb60ff9b11085aa1fe8
manuelfalonso <manuelalonso89@gmail.com>
2025-04-15 18:32:35 -0300

    Refactor InputManager and re implemented PlayerBehaviour inputs

--- Assets/LAGS/Scripts/Player/PlayerBehaviour.cs
+++ Assets/LAGS/Scripts/Player/PlayerBehaviour.cs
@@ -1,5 +1,6 @@
-using LAGS.Managers.Inputs;
+//using LAGS.Managers.Inputs;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
 namespace LAGS
 {
@@ -11,11 +12,12 @@ namespace LAGS
         [SerializeField] private Transform _dragFollowTarget;
 
         [Header("Properties")]
-        [SerializeField] private string _dragActionName;
+        //[SerializeField] private string _dragActionName;
+        [SerializeField] private UnityEngine.InputSystem.InputActionReference _dragActionReference;
 
         public Transform DragFollowTarget => _dragFollowTarget;
 
-        private ActionContexts _draggableContext;
+        //private ActionContexts _draggableContext;
 
         #region Unity Messages
         private void OnEnable()
@@ -34,12 +36,21 @@ namespace LAGS
         #region Private Methods
         private void SubscribeToInputs()
         {
-            _draggableContext = new ActionContexts
+            //_draggableContext = new ActionContexts
+            //{
+            //    ActionStarted = DragAction,
+            //    ActionCanceled = DropAction,
+            //};
+            //InputManager.Instance.SubscribeToAction(_dragActionName, _draggableContext);
+            
+            if (_dragActionReference == null)
             {
-                ActionStarted = DragAction,
-                ActionCanceled = DropAction,
-            };
-            InputManager.Instance.SubscribeToAction(_dragActionName, _draggableContext);
+                Debug.LogError("Missing reference", this);
+                return;
+            }
+
+            _dragActionReference.action.started += DragAction;
+            _dragActionReference.action.canceled += DropAction;
         }
 
         private void DragAction(UnityEngine.InputSystem.InputAction.CallbackContext context)
@@ -66,7 +77,16 @@ namespace LAGS
 
         private void UnsubscribeFromInputs()
         {
-            InputManager.Instance.UnsubscribeFromAction(_dragActionName, _draggableContext);
+            //InputManager.Instance.UnsubscribeFromAction(_dragActionName, _draggableContext);
+
+            if (_dragActionReference == null)
+            {
+                Debug.LogError("Missing reference", this);
+                return;
+            }
+
+            _dragActionReference.action.started -= DragAction;
+            _dragActionReference.action.canceled -= DropAction;
         }
         #endregion
     }
